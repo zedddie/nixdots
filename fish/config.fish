ffbbde59ff85aae3ee96c821098e8f54a9d0657e
zedddie <zedddiezxc@gmail.com>
2025-12-07 12:45:26 +0100

    add git & cargo abbr

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,15 @@ fish_vi_key_bindings insert
 if status is-login
 	keychain --quiet --eval $HOME/.ssh/id_ed25519 | source
 end
+abbr -a gst git status
+abbr -a g git 
+abbr -a gp git push
+abbr -a gf git fetch
+abbr -a gc git commit -m
+abbr -a ga git add
+
+abbr -a c cargo
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
