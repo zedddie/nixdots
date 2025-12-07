e69e08b031439a64ea2496b6222f1224c2600a88
zedddie <zedddiezxc@gmail.com>
2025-12-07 12:32:38 +0100

    add ssh agent keychain

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,12 @@
 set fish_greeting ""
+# if not set -q SSH_AGENT_PID
+# 	ssh-agent -c | source 
+# 	ssh-add ~/.ssh/id_ed25519
+# end
 fish_vi_key_bindings insert
+if status is-login
+	keychain --eval $HOME/.ssh/id_ed25519 | source
+end
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
