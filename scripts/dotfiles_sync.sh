d75dc606277bb8d86220ac951d85c4c867f4f2ca
zedddie <zedddiezxc@gmail.com>
2025-12-07 12:59:34 +0100

    dotfiles sync: 2025-12-07_12:59:32

---
+++ scripts/dotfiles_sync.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+cd ~/.config || exit 1
+
+NOW=$( date '+%F_%H:%M:%S' )
+DOTFILES_DIR="$HOME/nixdots/"
+
+cp -r fastfetch $DOTFILES_DIR
+cp -r ghostty $DOTFILES_DIR
+cp -r kitty $DOTFILES_DIR
+cp -r rofi $DOTFILES_DIR
+cp -r tmux $DOTFILES_DIR
+cp -r hypr $DOTFILES_DIR
+cp -r waybar $DOTFILES_DIR
+cp -r fish $DOTFILES_DIR
+sudo cp /etc/nixos/configuration.nix . 
+
+# cp $HOME/.config/Cursor/User/keybindings.json $DOTFILES_DIR/cursor/
+# cp $HOME/.config/Cursor/User/settings.json $DOTFILES_DIR/cursor/
+
+
+cd $DOTFILES_DIR
+git add .
+git commit -m "dotfiles sync: $NOW"
+git push
+
